60ce5d1cdbccd04feeaf9feade456b69d319bfb6
wyx208 <43093225+wyx208@users.noreply.github.com>
2018-10-12 10:07:45 +0800

    Add files via upload

---
+++ ex2-2.4.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.setup(650,350,200,200)
+turtle.penup()
+turtle.fd(-60)
+turtle.pendown()
+turtle.pensize(6)
+turtle.pencolor("red")
+turtle.seth(60)
+turtle.fd(120)
+turtle.seth(-60)
+turtle.fd(120)
+turtle.seth(-180)
+turtle.fd(120)
