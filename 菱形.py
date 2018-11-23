81670feffa54279396dbec168d136771c39af3e1
wyx208 <43093225+wyx208@users.noreply.github.com>
2018-11-23 10:34:06 +0800

    Add files via upload

---
+++ 菱形.py
@@ -0,0 +1,10 @@
+import turtle as t
+t.penup()
+t.fd(-200)
+t.pendown()
+t.seth(30)
+for i in range(2):
+    t.fd(200)
+    t.right(60)
+    t.fd(200)
+    t.right(120)
---
+++ 输入一个整数.py
@@ -0,0 +1,5 @@
+s=eval(input("输入一个整数："))
+try:
+    num=eval(input("请输入一个整数"))
+except NameError:
+    print("输入错误，请输入一个整数")
