81bd951c37698bccbfff6f36080ddf89d5a47f8b
wyx208 <43093225+wyx208@users.noreply.github.com>
2018-11-23 08:45:28 +0800

    Add files via upload

--- ex5-5.5.py
+++ ex5-5.5.py
@@ -1,7 +1,25 @@
 def isPrime(x):
-    for i in range(2,int(math.sqrt(x))+1):
+    for i in range(2,x):
         if x % i ==0:
             return False
             break
     else:
         return True
+while True:
+    try:
+        n = eval(input("Enter an Integer: "))
+    except:
+        print("Enter error! Please enter again: ")
+        continue
+    
+    if type(n) is not int:
+        print("Please enter an Integer: ")
+        continue
+    if n == -1:
+        break
+        
+    if isPrime(n):
+        print("{} is prime.".format(n))
+    else:
+        print("{} is not prime.".format(n))
+
